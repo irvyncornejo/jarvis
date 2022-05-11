949415394524c91ff1cdb4faca804e0094f9ae48
Irvyn Cornejo <irvyncornejo@gmail.com>
2022-05-11 00:28:38 -0500

    Inicio de la app con flask

--- iza/app/db.py
+++
@@ -1,9 +0,0 @@
-import os
-from firebase import firebase
-
-_URL_DB = os.environ['BD_FIREBASE']
-firebase = firebase.FirebaseApplication(_URL_DB, None)
-
-result = firebase.get('/device/sensores', None)
-print(type(result))
-
