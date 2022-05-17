69066fbef2774859cb51617fc4c490469b1eb299
Irvyn Cornejo <irvyncornejo@gmail.com>
2022-05-17 00:03:40 -0500

    Primer programa para escuchar las instrucciones

--- iza/voice-recognition.py
+++ iza/voice-recognition.py
@@ -1,11 +1,15 @@
 import speech_recognition as sr
+from time import sleep
 
 lister = sr.Recognizer()
 
-try:
-    with sr.Microphone() as source:
-        voice = lister.listen(source)
-        command = lister.recognize_google(voice)
-        print(command)
-except Exception as e:
-    print(e)
+if __name__=='__main__':
+    while True:
+        try:
+            sleep(1)
+            with sr.Microphone() as source:
+                voice = lister.listen(source)
+                command = lister.recognize_google(voice)
+                print(command)
+        except Exception as e:
+            pass
